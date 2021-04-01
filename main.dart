cf2ddb905627df51e15435f93f7068ddc803e74e
AnggaPermana2 <71532932+AnggaPermana2@users.noreply.github.com>
2021-04-01 22:52:32 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
+import 'bmi.dart';
 
+void main() => runApp(MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blueAccent,
+        // brightness: Brightness.dark                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+      ),
+      home: InputBMI(),
+    ));
